92f1c1fdbfd8dc6560d9114996f7153e7e0fd5e4
mosegic <39779935+mosegic@users.noreply.github.com>
2026-09-05 16:04:53 +0300

    Refactor Sign In button structure in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,14 +13,7 @@ export default function HomePage() {
        
         
         </div>
-      <div className="flex gap-3">
-  <Button variant="ghost" asChild>
-    <Link href="/login">Sign In</Link>
-  </Button>
-  <Button variant="default" asChild>
-    <Link href="/register">Sign Up</Link>
-  </Button>
-</div>
+     <div className="flex gap-3"> <Link href="/login"> <Button variant="ghost">Sign In</Button> </Link> </div>
 
 
 
